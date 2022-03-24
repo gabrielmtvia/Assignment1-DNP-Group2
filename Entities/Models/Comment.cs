484039526f78537599a3045f03a80f05916f412f
hamoudi8080 <82207003+hamoudi8080@users.noreply.github.com>
2022-03-24 16:40:51 +0100

    classes added

---
+++ Entities/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Models;
+
+namespace Assigntment1.models;
+
+public class Comment
+{
+    
+    public User Body
+    {
+        set;
+        get;
+    }
+    
+    public User WrittenBy
+    {
+        set;
+        get;
+    }
+}
---
+++ Entities/Models/SubForum.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Models;
+
+namespace Assigntment1.models;
+
+public class SubForum
+{
+    public User OwnedBy
+    {
+        set;
+        get;
+    }
+    public String Title
+    {
+        set;
+        get;
+    }
+
+    public String Description
+    {
+        set;
+        get;
+    }
+
+    public SubForum(  string title, string description)
+    {
+        
+        Title = title;
+        Description = description;
+    }
+
+    public ICollection<Post> Posts { get; set; }
+}
